bfea65b74c730ed7409b681a65d974d3a8977538
Dimitry-prog <kfifa3@gmail.com>
2022-08-03 17:20:12 +0300

    adeed files

---
+++ scripts/delivery.js
@@ -0,0 +1,12 @@
+
+
+const deliverySwiper = new Swiper(".swiper__delivery", {
+  spaceBetween: 30,
+  loop: 'true',
+  autoheght: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: 'progressbar',
+  },
+})
